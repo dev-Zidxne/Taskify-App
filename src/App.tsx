8d1f31fde458aa6dee76ded91d5c16415c89743b
Zidane Innis <zidaneinnis@gmail.com>
2022-12-03 11:12:08 -0500

    Learning type declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let name: string;
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person = {
+  name: "Zidane",
+};
+
+let lotsOfPeople: Person;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello</div>;
 }
 
 export default App;
