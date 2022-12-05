6da4401f75af7a814dded0657b9df71ba3c5d508
Zidane Innis <zidaneinnis@gmail.com>
2022-12-05 12:16:50 -0500

    Major updates

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-function InputField({ todo, setTodo }: Props) {
+function InputField({ todo, setTodo, handleAdd }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+        ref={inputRef}
+        value={todo}
         type="input"
         placeholder="Enter as task"
         className="input__box"
+        onChange={(e) => setTodo(e.target.value)}
       ></input>
       <button className="input_submit" type="submit">
         Go
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import { Todo } from "../model";
+import "./styles.css";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+export const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  return (
+    <form className="todos__single">
+      <span className="todos--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
