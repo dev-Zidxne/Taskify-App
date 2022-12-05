8ed9fa4374a9ce0a1c05a5e153fecc2d4288a79e
Zidane Innis <zidaneinnis@gmail.com>
2022-12-05 08:32:54 -0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-let name: string;
-let age: number;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
-
-type Person = {
-  name: string;
-  age?: number;
-};
-
-let person: Person = {
-  name: "Zidane",
-};
-
-let lotsOfPeople: Person;
-
 function App() {
   return <div className="App">Hello</div>;
 }
